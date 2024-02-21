4c7f48b9305e5410ab44b39f37d620aaed4a3209
belopa <78800892+belopa@users.noreply.github.com>
2024-02-21 11:47:38 +0000

    Update Function_Lambert_Solver.m

--- Function_Lambert_Solver.m
+++ Function_Lambert_Solver.m
@@ -10,49 +10,70 @@ function [Velocity_Vector1,Velocity_Vector2, Elements] = Function_Lambert_Solver
 %%document.
 
     %%%%%%%%%ALL OF THESE EQUATIONS CAN BE FOUND IN "ORBITAL MECHANICS FOR
-                        %%%%%%%%%ENGINEERING STUDENTS"            
+                        %%%%%%%%%ENGINEERING STUDENTS" 
 
-%% Step 1&2:  Calculate the norm of the position vectors to give the "magnitude"
-%% Calculate the change in true anomaly 
+%Inputs of funciton:
+    % z = initial guess for Newton's iteration
+    % mu = Standard gravitational parameter of the Sun [km^3/s^2]
+    % R1 = position of departure planet
+    % R2 = position of arrival planet 
+    % delta_t = tof (time of flight)
+    % string = 'pro' or 'retro'
+%Outputs of function:
+    % Velocity_Vector1 = velocity required by satellite at departure planet
+    % Velocity_Vector2 = velocity required by satellite at arrival planet
 
+%% Step 1:  Calculate the norm, dot and cross product of the position vectors
 r1 = norm(R1);
 r2 = norm(R2);
-dot_product = dot(R1,R2_inv);
-multiple_position = norm(R1)*norm(R2_inv);
-c12 = cross(R1, R2);
-division_position = (dot_product./multiple_position');
-sigma = acos(division_position);                         % Change in true anomaly (Eq 5.23) 
+dot_product = dot(R1,R2);
+cross_product = cross(R1,R2);
 
-
-
+%% Step 2: Calculate the change in true anomaly (sigma)
 % Checking whether the orbit is prograde or retrograde to change the TA accordingly (Eq
 % 5.26)
-if strcmp(string,'pro')
-   if c12(3) <= 0
-        sigma = 2*pi - sigma;
-   end
-elseif strcmp(string,'retro')
-   if c12(3) >= 0
-        sigma = 2*pi - sigma;
-   end
+
+if strcmp(string, 'pro')
+
+    if cross_product(:,3) >= 0
+        sigma =  acos((dot_product)./(r1*r2));
+    else  
+        sigma = (2*pi) - acos((dot_product)./(r1*r2));
+    end
+
+elseif strcmp(string, 'retro')
+
+    if cross_product(:,3) < 0
+        sigma =  acos((dot_product)./(r1*r2));
+    else  
+        sigma = (2*pi) - acos((dot_product)./(r1*r2));
+    end
+
 end
 
 %% Step 3: Simplification expression  
 A = sin(sigma).*sqrt((r1*r2)./(1-cos(sigma)));        % (Eq 5.35)
 
+%% Step 4: Find the rough value of z where F = 0
+F_something = F(z,delta_t);                           % Initial value of F using initial z and tof values 
 
-%% Step 4: Calculate what the value of z is using initial guess given in Step 4
+while F_something < 0
+    z = z + 0.1;
+    F_something = F(z,delta_t);
+end
+
+%% Step 5: Calculate what the value of z is using initial guess given in Step 4
 % (Newton's procedure Pg 209)
 
-tol       = 1e-3;          %Tolerance
-nmax      = 100000;        %Limit on iterations
-F_ratio   = 0.001;
+tol       = 1e-3;                    %Tolerance
+nmax      = 1000000;                 %Limit on iterations
+F_ratio   = F_something/F_der(z);    %Initial value of F_ratio
 counter_2 = 0;
 
 while abs(F_ratio) > tol && counter_2 <= nmax
-    counter_2 = counter_2 + 1
-    F_ratio   = F(z,delta_t)/F_der(z);
     z         = z - F_ratio;
+    F_ratio   = F(z,delta_t)/F_der(z);
+    counter_2 = counter_2 + 1;
 end
 
 % Display error if the maximum number of iterations is exceeded:
@@ -61,10 +82,10 @@ if counter_2 >= nmax
 end
 
 
-%% Step 5: Calculating Lagrange function (Expressed in terms of universal anomaly for this analysis)
-f     = 1 - (y(z)./r1);            % Lagrange function (Eq 5.46a)
-g     = A.*sqrt(y(z)./mu);         % Lagrange function (Eq 5.46b)
-g_dot = 1 - (y(z)./r2);            % Lagrange function (Eq 5.46d)
+%% Step 5: Calculating Lagrange coefficients (Expressed in terms of universal anomaly for this analysis)
+f     = 1 - (Y(z)./r1);            % Lagrange function (Eq 5.46a)
+g     = A.*sqrt(Y(z)./mu);         % Lagrange function (Eq 5.46b)
+g_dot = 1 - (Y(z)./r2);            % Lagrange function (Eq 5.46d)
 
 %% Step 6: Calculating Velocity Vectors (Rearranging the relation between Lagrange coefficients and position&velocity vectors)
 
@@ -75,50 +96,73 @@ R = [R1 R2];                                        % Position Vector
 V = [Velocity_Vector1 Velocity_Vector2];            % Velocity Vector
 
 
-%% Step 7: Converting Position Vectors into Keplerian Elements
+%% Step 7: Converting Position Vectors into Keplerian Elements 
+% https://downloads.rene-schwarz.com/download/M002-Cartesian_State_Vectors_to_Keplerian_Orbit_Elements.pdf
 
-r     = norm(R1);
-v     = norm(Velocity_Vector1);
-v_r   = dot(R1/r, Velocity_Vector1);
-v_p   = sqrt(v^2 - v_r^2);
-h_vec = cross(R1, Velocity_Vector1);
-h     = norm(h_vec);                                                % Angular momentum
-i     = rad2deg(acos(h_vec(3) / h));                                % Inclination
-K     = [0, 0, 1];  
-N_vec = cross(K, h_vec);        
-N     = norm(N_vec);    
-Omega = 360 - rad2deg(2 * pi - acos(N_vec(1)/N));                   % RAAN
-e_vec = (cross(Velocity_Vector1, h_vec) / mu) - (R1 / r);
-e     = norm(e_vec);                                                % Eccentricity
-w     = 360 - rad2deg(2 * pi - acos(dot(N_vec, e_vec) / (N * e)));  % Argument of Perigee
-nu    = 360 - rad2deg(acos(dot(e_vec, R1) / (e * r)));              % Mean Anomaly
-a     = h^2/(mu*(1 - e^2));                                         % Semi-major axis
+r1     = norm(R1);
+v1     = norm(Velocity_Vector1);
+h      = cross(R1,Velocity_Vector1);                             % Orbital momentum vector
+e_vec  = ((cross(Velocity_Vector1,h))/mu) - (R1/norm(R1));       % Eccentric vector 
+e      = norm(e_vec);                                            % e scalar
+E      = 2 * atan( (tan(v/2)) / (sqrt((1+e)/(1-e))) );           % Eccentric anomaly
+i      = (acos(h(3) / norm(h)));                                 % Inclination
+M0     = E - e*sin(E);                                           % Mean anomlay
+a      = 1/ ( (2/r1) - (v1^2/mu) );                              % Semi-major axis
+T      = 2 * pi * sqrt((a^3)/mu);                                % Period
 
-Elements = [a e i Omega w nu];
+k     = [0 0 1];
+n     = cross(k', h);                                            % Vector that points towards the ascending node
 
+%True anomaly
+if dot(R1, Velocity_Vector1) >= 0                              
+    v = acos((dot(R1,Velocity_Vector1))/(r1*v1));
+else
+    v = 2*pi - acos((dot(R1,Velocity_Vector1))/(r1*v1));
+end
+
+% Ascending node
+if n(2)>= 0 
+    O = acos(n(1)/norm(n));                                
+else
+    O = 2*pi - acos(n(1)/norm(n));
+end
+
+%Argument of pariapsis 
+if e_vec(3) >= 0 
+    w = acos((dot(n,e_vec))/(norm(n)*e));                   
+else
+    w = 2*pi - acos((dot(n,e_vec))/(norm(n)*e));
+end
+
+Elements = [a e i O w M0];
+
+
+% Intent to get rid of values that give imaginary semi major axis:
+% mask = imag(T) == 0;
+% T_real = T(mask,:);
 
 
 %--------------------------------------------------------------------%
                             %FUNCTIONS%
 
 %% FUNCTION 1: Value of y (Equation 5.38)
-function Value_y = y(z)
+function Value_y = Y(z)
 Value_y = r1 + r2 + (A.*(((z.*S(z))-1)./sqrt(C(z))));
 end
 
 %% FUNCTION 2: Value of F(z) function (Equation 5.40)
 function F_function = F(z, delta_t)
-    F_function = (((y(z)./C(z)).^1.5).*S(z)) + A.*sqrt(y(z)) - (sqrt(mu)*delta_t);
+    F_function = (((Y(z)./C(z)).^1.5).*S(z)) + A.*sqrt(Y(z)) - (sqrt(mu)*delta_t);
 end
 
 %% FUNCTION 3: Value of derivative of F(z) function (Equation 5.43)
 function Derivative_F = F_der(z)
 if z == 0 
-    Derivative_F = ((sqrt(2)/40)*(y(0)^1.5)) + (A/8).*(sqrt(y(0)) + A.*sqrt(1/(2.*y(0))));
+    Derivative_F = ((sqrt(2)/40)*(Y(0)^1.5)) + (A/8).*(sqrt(Y(0)) + A.*sqrt(1/(2.*Y(0))));
 else
-    Derivative_F = ((y(z)./C(z)).^1.5) * ((1./(2.*z)).*(C(z) - (3.*S(z))/(2.*C(z))) ...
-          + (3.*(S(z).^2))./(4.*C(z))) + A/.8.*(((3.*S(z))./C(z)).*sqrt(y(z)) ...
-          + A.*sqrt(C(z)./y(z)));
+    Derivative_F = ((Y(z)./C(z)).^1.5) * ((1./(2.*z)).*(C(z) - (3.*S(z))/(2.*C(z))) ...
+          + (3.*(S(z).^2))./(4.*C(z))) + A/.8.*(((3.*S(z))./C(z)).*sqrt(Y(z)) ...
+          + A.*sqrt(C(z)./Y(z)));
 end
 
 end
@@ -128,7 +172,7 @@ function StumpffFunction_S = S(z)
 if z>0
     StumpffFunction_S = (sqrt(z)-sin(sqrt(z)))/((sqrt(z))^3);
 elseif z < 0 
-    StumpffFunction_S = (sinh(sqrt(-z))-sqrt(-z))/((sqrt(z))^3);
+    StumpffFunction_S = (sinh(sqrt(-z))-sqrt(-z))/((sqrt(-z))^3);
 else
     StumpffFunction_S = 1/6;
 end
@@ -143,8 +187,6 @@ elseif z < 0
 else
     StumpffFunction_C = 1/2;
 end
-end
-
 end
 
 end
